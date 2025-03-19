43a3139073a17b1c9337185cdbf423bec7cb3704
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:39:51 +0700

    menambahkan class SimpleDataBase

---
+++ PemrosesData.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300084
+{
+    internal class PemrosesData
+    {
+        public void DapatkanNilaiTerbesar<T>(T data1, T data2, T data3)
+        {
+            dynamic temp1 = data1;
+            dynamic temp2 = data2;
+            dynamic temp3 = data3;
+
+            if (temp1 > temp2 && temp1 > temp3)
+            {
+                temp1 = data1;
+            }
+            else if (temp2 > temp1 && temp2 > temp3)
+            {
+                temp1 = data2;
+            }
+            else 
+            {
+                temp1 = data3; 
+            }
+
+            Console.WriteLine("Nilai terbesar: "+temp1);
+        }
+    }
+
+
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+        }
+        public void AddNewData(T data) 
+        { 
+            storedData.Add(data);
+            inputDates.Add(DateTime.Now);
+        }
+        public void PrintAllData() {
+            for (int i = 0; i < storedData.Count; i++) 
+            {
+                Console.WriteLine("Data " + i + " berisi: " + storedData[i] +
+                    ", yang disimpang pada waktu UTC: " + inputDates[i]);    
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_103022300084;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PemrosesData data = new PemrosesData();
+        data.DapatkanNilaiTerbesar(1030, 2230, 0084);
+        
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddNewData(1030);
+        db.AddNewData(2230);
+        db.AddNewData(0084);
+        db.PrintAllData();
+
+        SimpleDataBase<String> dataString = new SimpleDataBase<String>();
+        dataString.PrintAllData();
+    }
+}
